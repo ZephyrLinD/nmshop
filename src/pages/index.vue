e521617625cef47d29bffc7e2bf9cb85734aa262
ZephyrLin <zephrdod@gmail.com>
2020-09-12 17:20:41 +0800

    轮播图旁边的商品展示部分

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,11 +2,49 @@
   <div class="index">
     <div class="container">
       <div class="swiper-box">
+        <div class="nav-menu">
+          <ul class="menu-wrap">
+            <li class="menu-item">
+              <a href="javascript:;">传感器</a>
+              <div class="children">
+                <ul v-for="(item,i) in menuList" v-bind:key="i">
+                  <li v-for="(sub,j) in item" v-bind:key="j">
+                    <a v-bind:href="sub?'/#/product/'+sub.id:''">
+                      <img v-bind:src="sub?sub.img:'/imgs/item-box-1.png'" alt="">
+                      {{sub?sub.name:'小米9'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">软路由</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">NAS</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">智能硬件</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">开发板</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">解决方案</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">占位符</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">占位符2</a>
+            </li>
+          </ul>
+        </div>
         <swiper :options="swiperOption">
           <swiper-slide v-for="(item, index) in slideList" :key="index">
-                <a :href="'/#/product/'+item.id">
-                    <img :src="item.img" />
-                </a>
+            <a :href="'/#/product/'+item.id">
+              <img :src="item.img" />
+            </a>
           </swiper-slide>
           <div class="swiper-pagination" slot="pagination"></div>
           <div class="swiper-button-prev" slot="button-prev"></div>
@@ -34,23 +72,23 @@ export default {
   },
   data() {
     return {
-        swiperOption: {
-            autoplay: true,
-            loop: true,
-            effect: 'cube',
-            cubeEffect: {
-                  shadowOffset: 100,
-                  shadowScale: 0.6
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev'
-            }
+      swiperOption: {
+        autoplay: true,
+        loop: true,
+        effect: "cube",
+        cubeEffect: {
+          shadowOffset: 100,
+          shadowScale: 0.6,
         },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
       slideList: [
         {
           id: "42",
@@ -73,21 +111,121 @@ export default {
           img: "/imgs/slider/slide-5.jpg",
         },
       ],
+      menuList: [
+        [
+          {
+            id: 30,
+            img: "/imgs/item-box-1.png",
+            name: "小米CC9",
+          },
+          {
+            id: 31,
+            img: "/imgs/item-box-2.png",
+            name: "小米8青春版",
+          },
+          {
+            id: 31,
+            img: "/imgs/item-box-3.jpg",
+            name: "Redmi K20 PRO",
+          },
+          {
+            id: 33,
+            img: "/imgs/item-box-4.jpg",
+            name: "移动4g专区",
+          },
+        ],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+      ],
     };
   },
 };
 </script>
 
 <style lang="scss">
+@import "./../assets/scss/mixin.scss";
+@import "./../assets/scss/config.scss";
 .index {
-    .swiper-box {
-        .swiper-container {
-            height: 451px;
-            img {
-                width: 100%;
-                height: 100%;
+  .swiper-box {
+    .nav-menu {
+      position: absolute;
+      width: 264px;
+      height: 451px;
+      z-index: 9;
+      padding: 26px 0;
+      background-color: #55585a7a;
+      box-sizing: border-box;
+      .menu-wrap {
+        .menu-item {
+          height: 50px;
+          line-height: 50px;
+          a {
+            position: relative;
+            display: block;
+            font-size: 16px;
+            color: #fff;
+            padding-left: 30px;
+            &:after {
+              position: absolute;
+              right: 30px;
+              top: 17.5px;
+              content: " ";
+              @include bgImg(10px, 15px, "/imgs/icon-arrow.png");
             }
+          }
+          &:hover {
+            background-color: $colorA;
+            .children {
+                display: block;
+            }
+          }
+          .children {
+              display: none;
+              width: 962px;
+              height: 451px;
+              background-color: $colorG;
+              position: absolute;
+              top: 0;
+              left: 264px;
+              border: 1px solid $colorH;
+              ul {
+                  display: flex;
+                  justify-content: space-between;
+                  height: 75px;
+                  li {
+                      height: 75px;
+                      line-height: 75px;
+                      width: 241px;
+                      padding-left: 23px;
+                  }
+                  a {
+                      color: $colorB;
+                      font-size: 14;
+                  }
+                  img {
+                      width: 42px;
+                      height: 35px;
+                      vertical-align: middle;
+                      margin-right: 15px;
+                  }
+              }
+          }
         }
+      }
+    }
+    .swiper-container {
+      height: 451px;
+      .swiper-button-prev {
+        left: 274px;
+      }
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
   }
 }
 </style>
