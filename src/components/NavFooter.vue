dbd95de29633f9ed039e4904d2eec89d06f6e157
ZephyrLin <zephrdod@gmail.com>
2020-09-12 11:26:19 +0800

    service bar

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -5,10 +5,10 @@
             <p>纳米商城</p>
         </div>
         <div class="footer-link">
-            <a href="javascript:;">卧槽</a><span>|</span>
-            <a href="javascript:;">卧槽</a><span>|</span>
-            <a href="javascript:;">卧槽</a><span>|</span>
-            <a href="javascript:;">卧槽</a>
+            <a href="https://github.com/zephyrlind" target="_blank">关于我们</a><span>|</span>
+            <a href="javascript:;">帮助中心</a><span>|</span>
+            <a href="javascript:;">售后服务</a><span>|</span>
+            <a href="javascript:;">关于货源</a><span>|</span>
         </div>
         <div class="copyright">Copyright ©2020 <span class="domain">nmshop.com</span> All Rights Reserved.</div>
     </div>
@@ -35,6 +35,7 @@ export default {
         img {
             width: 53px;
             height: 36px;
+            margin-bottom:13px;
         }
     }
     .footer-link {
