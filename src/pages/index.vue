d4d6c8b83492a0002bd085731e38af5538572eaa
ZephyrLin <zephrdod@gmail.com>
2020-09-15 14:20:54 +0800

    首页的广告位、banner

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -51,8 +51,16 @@
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <div class="ads"></div>
-      <div class="banner"></div>
+      <div class="ads-box">
+        <a :href="'/#/product/'+item.id" v-for="(item,index) in adsList" :key=index>
+          <img :src="item.img" alt="">
+        </a>
+      </div>
+      <div class="banner">
+        <a href="/#/product/30">
+          <img src="/imgs/banner-1.png" alt="">
+        </a>
+      </div>
       <div class="product-box"></div>
     </div>
     <service-bar></service-bar>
@@ -140,6 +148,21 @@ export default {
         [1, 1, 1, 1],
         [1, 1, 1, 1],
       ],
+      adsList: [
+        {
+          id: 33,
+          img: '/imgs/ads/ads-1.png'
+        }, {
+          id: 48,
+          img: '/imgs/ads/ads-2.jpg'
+        }, {
+          id: 45,
+          img: '/imgs/ads/ads-3.png'
+        }, {
+          id: 47,
+          img: '/imgs/ads/ads-4.jpg'
+        }, 
+      ]
     };
   },
 };
@@ -226,6 +249,18 @@ export default {
         height: 100%;
       }
     }
+  }
+  .ads-box {
+    @include flex();
+    margin-top: 14px;
+    margin-bottom: 41px;
+    a {
+      width: 296px;
+      height: 167px;
+    }
+  }
+  .banner {
+    margin-bottom: 50px;
   }
 }
 </style>
