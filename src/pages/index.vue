4ef7b4f06043b0daba51d8de5dd2f8d9061f602a
ZephyrLin <zephrdod@gmail.com>
2020-09-15 17:22:50 +0800

    封装modal

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -87,11 +87,20 @@
       </div>
     </div>
     <service-bar></service-bar>
+    <modal title="提示" 
+      sureText="查看购物车" 
+      btnType="1" 
+      modalType="middle" :showModal="true">
+      <template v-slot:body>
+        <p>商品添加成功</p>
+      </template>
+      </modal>
   </div>
 </template>
 
 <script>
 import ServiceBar from "./../components/ServiceBar";
+import Modal from "./../components/Modal";
 import { Swiper, SwiperSlide } from "vue-awesome-swiper";
 import "swiper/css/swiper.css";
 export default {
@@ -100,6 +109,7 @@ export default {
     Swiper,
     SwiperSlide,
     ServiceBar,
+    Modal
   },
   data() {
     return {
