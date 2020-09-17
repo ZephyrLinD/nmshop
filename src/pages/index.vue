d869798ba4ab69a6d99d13e329ac613720962105
ZephyrLin <zephrdod@gmail.com>
2020-09-17 09:14:47 +0800

    Modal动画

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -78,7 +78,7 @@
                 <div class="item-info">
                   <h3>{{item.name}}</h3>
                   <p>{{item.subtitle}}</p>
-                  <p class="price">{{item.price}}元</p>
+                  <p class="price" @click="addCart(item.id)">{{item.price}}元</p>
                 </div>
               </div>
             </div>
@@ -90,7 +90,11 @@
     <modal title="提示" 
       sureText="查看购物车" 
       btnType="1" 
-      modalType="middle" :showModal="true">
+      modalType="middle" 
+      :showModal="showModal"
+      v-on:submit="goToCart"
+      v-on:cancel="showModal = false"
+      >
       <template v-slot:body>
         <p>商品添加成功</p>
       </template>
@@ -214,6 +218,20 @@ export default {
         res.list = res.list.slice(6, 14);
         this.sensorList = [res.list.slice(0, 4), res.list.slice(4, 8)];
       })
+    },
+    addCart() {
+      // this.axios.port('/carts', {
+      //   productId: id,
+      //   selected: true
+      // }).then(() => {
+
+      // }).catch(() => {
+      //   this.modal = true
+      // })
+      this.modal = true
+    },
+    goToCart() {
+      this.$router.push('/cart');
     }
   }
 
