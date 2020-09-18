ab6e9790fb1f756ac4713d085bc3faa6e666b448
ZephyrLin <zephrdod@gmail.com>
2020-09-18 15:31:52 +0800

    商品页面导航栏吸顶

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,11 +1,41 @@
 <template>
-    <div>
-        product
+    <div class="product">
+        <product-param>
+            <template v-slot:buy>
+                <button class="btn">立即购买</button>
+            </template>
+        </product-param>
+        <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
     </div>
+    
 </template>
 
 <script>
+import ProductParam from './../components/ProductParam'
 export default {
     name: 'product',
+    components: {
+        ProductParam
+    }
 }
 </script>
+
+<style lang="scss">
+    .h {
+        height: 150px;
+    }
+    .product {
+        button {
+            margin-left: 10px;
+        }
+    }
+</style>
