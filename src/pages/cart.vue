0a5f757da94cc7f99b147c49204b41964c6c49f8
ZephyrLin <zephrdod@gmail.com>
2020-09-19 23:23:01 +0800

    购物车订单确认，购物车基本完成

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -45,7 +45,7 @@
           </div>
           <div class="total fr">
             合计：<span>{{cartTotalPrice}}</span>元
-            <a href="javascript:;" class="btn">去结算</a>
+            <a href="javascript:;" class="btn" @click="order">去结算</a>
           </div>
         </div>
       </div>
@@ -127,6 +127,14 @@
           this.axios.delete(`/carts/${item.productId}`).then((res) => {
               this.renderData(res);
           })
+      },
+      order() {
+          let isCheck = this.list.every(item=>!item.productSelected);
+          if(isCheck) {
+              alert('请选择一件商品');
+          } else {
+              this.$router.push('/order/confirm');
+          }
       }
     }
   }
