c2d4520c997566e90f8b7c2fafb7fe01e48a5b09
ZephyrLin <zephrdod@gmail.com>
2020-09-19 17:02:19 +0800

    Order-header

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -1,9 +1,60 @@
 <template>
-    <div>order-header</div>
+    <div class="order-header">
+        <div class="container clearfix">
+            <div class="header-logo">
+                <a href="/#/index"></a>
+            </div>
+            <div class="title">
+                <h2>{{title}}<slot name="tip"></slot></h2>
+            </div>
+            <div class="username">
+                <a href="javascript:;">zep</a>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'order-header'
+    name: 'order-header',
+    props: {
+        title: String
+    }
 }
 </script>
+
+<style lang="scss">
+    @import './../assets/scss/reset.scss';
+    .order-header {
+        padding: 30px 0;
+        .header-logo {
+            float: left;
+        }
+        .title, .username{
+            display: inline-block;
+            height: 55px;
+            line-height: 55px;   
+        }
+        .title {
+            float: left;
+            color: #333;
+            margin-left: 54px;
+            h2 {
+                font-size: 28px;
+            }
+            span {
+                font-size: 14px;
+                margin-left: 17px;
+                color: #999;
+                font-weight: bold;
+            }
+        }
+        .username {
+            float: right;
+            a {
+                color: #666;
+                font-size: 16px;
+            }
+        }
+    }
+</style>
